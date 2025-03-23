a995189c1f1437733629f334360985b48ee6740c
Anup Saud <anupsaud10@gmail.com>
2025-03-22 21:15:44 -0400

    Inital Project folder

---
+++ XKeymap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XKeymap
+//
+//  Created by Anup Saud on 2025-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XKeymap/XKeymapApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKeymapApp.swift
+//  XKeymap
+//
+//  Created by Anup Saud on 2025-03-22.
+//
+
+import SwiftUI
+
+@main
+struct XKeymapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
